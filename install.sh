781a0296c14807c2617c27fa8eba3dd71f8cad29
Charmik <charmik1994@gmail.com>
2024-09-08 17:27:45 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,31 @@
-set -ex
+set -e
 
 
+services=("opentopodata.service" "autoroute.service")
 
-cp XXX.service /etc/systemd/system/
+# Iterate over the array and print each service
+for service in "${services[@]}"; do
+    cp $service /etc/systemd/system/"$service"
+    systemctl daemon-reload
+    systemctl enable "$service"
+    systemctl start "$service"
+    sleep 10
+done
 
-sudo systemctl status autoroute
-sudo systemctl start autoroute
+
+#cp XXX.service /etc/systemd/system/
+#
+#sudo systemctl status autoroute
+#sudo systemctl start autoroute
 #sudo systemctl restart autoroute
 
 
 #elevation service
 #cd ~/data/opentopodata && make build && make run
 
-sudo systemctl daemon-reload  # Reload systemd to recognize the new service
-sudo systemctl enable opentopodata.service  # Enable the service to start on boot
-sudo systemctl start opentopodata.service   # Start the service immediately
+#sudo systemctl daemon-reload  # Reload systemd to recognize the new service
+#sudo systemctl enable opentopodata.service  # Enable the service to start on boot
+#sudo systemctl start opentopodata.service   # Start the service immediately
 #journalctl -u opentopodata.service -f
 
 
