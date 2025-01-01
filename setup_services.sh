1f0dc15d396c2dcc15f5519d19cfa9ec7062691b
Charmik <charmik1994@gmail.com>
2025-01-01 20:58:35 +0200

    add cd & echo

--- setup_services.sh
+++ setup_services.sh
@@ -20,7 +20,9 @@ else
   echo "aster30m" exists
 fi
 
+cd /home/charm/data
 if [ ! -d "photon" ]; then
+  echo "photon doesnt exist, clone the project"
   git clone https://github.com/komoot/photon
   cd photon
   sudo apt-get install bzip2 pbzip2
