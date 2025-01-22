fcb5c48f0ec5fac788c3c1f664366263576e6c8d
Charmik <charmik1994@gmail.com>
2025-01-22 12:21:08 +0200

    fixes

--- init.sh
+++ init.sh
@@ -21,18 +21,33 @@ sudo apt-get -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+DISTRO=$(grep "^ID=" /etc/os-release | cut -d '=' -f 2)
+
+if [[ "$DISTRO" == "ubuntu" ]]; then
+  echo "Using Ubuntu Docker repository"
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -c | awk '{print $2}') stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+elif [[ "$DISTRO" == "debian" ]]; then
+  echo "Using Debian Docker repository"
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(lsb_release -c | awk '{print $2}') stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+else
+  echo "Unsupported distribution: $DISTRO"
+  exit 1
+fi
+
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo rm /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl start docker
 sudo usermod -aG docker $USER
 sudo docker run hello-world
 
 # Valhalla
-cd ~/data
+cd ~/disk
 if [ ! -d "docker-valhalla" ]; then
   git clone https://github.com/gis-ops/docker-valhalla
 fi
@@ -45,7 +60,7 @@ fi
 # docker compose -f docker-compose-build.yml up --build
 
 # OpenTopoData
-cd ~/data
+cd ~/disk
 if [ ! -d "opentopodata" ]; then
   git clone https://github.com/Charmik/opentopodata
 fi
