8fa57a4d34140aa3e6e99fd716c2620f856d4fbc
Charmik <charmik1994@gmail.com>
2025-01-22 12:26:23 +0200

    add -f for remove

--- init.sh
+++ init.sh
@@ -39,7 +39,7 @@ fi
 
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo rm /usr/local/bin/docker-compose
+sudo rm -f /usr/local/bin/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl start docker
