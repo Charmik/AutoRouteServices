ff505bdd1cdce860dba0608bf66fd7ca78aa185a
Charmik <charmik1994@gmail.com>
2025-05-15 13:02:01 +0300

    build and run OSRM with CH alg

--- osrm/build_full.sh
+++ osrm/build_full.sh
@@ -8,8 +8,7 @@ cp ~/data/AutoRouteServices/osrm/bicycle.lua .
 curl -OL https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf
 
 docker run --rm -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-extract -p /data/bicycle.lua /data/planet-latest.osm.pbf || echo "osrm-extract failed"
-docker run --rm -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-partition /data/planet-latest.osrm || echo "osrm-partition failed"
-docker run --rm -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-customize /data/planet-latest.osrm || echo "osrm-customize failed"
+docker run --rm -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-contract /data/planet-latest.osrm || echo "osrm-contract failed"
 date
 telegram-send "Finished osrm build $(hostname)"
 rm planet-latest.osm.pbf
