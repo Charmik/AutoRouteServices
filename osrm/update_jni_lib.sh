e5717d5dca11aa9300fbd0316a1b88f155cedf47
Charmik <charmik1994@gmail.com>
2026-04-16 18:52:43 +0300

    save old target to old

--- osrm/update_jni_lib.sh
+++ osrm/update_jni_lib.sh
@@ -3,6 +3,7 @@ set -ex
 cd ~/disk/AutoRoute
 rm -rf target
 ./build_osrm_jni.sh
-rm -rf ~/data/AutoRoute/target
+rm -rf ~/data/AutoRoute/target_old
+mv ~/data/AutoRoute/target ~/data/AutoRoute/target_old
 mkdir -p ~/data/AutoRoute/target/lib
 cp target/lib/libosrmjni.so ~/data/AutoRoute/target/lib
